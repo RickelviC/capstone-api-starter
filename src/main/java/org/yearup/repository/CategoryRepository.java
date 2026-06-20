8bdfcad8a1ef1943b86862f8d79a797226d62352
RickelviC <Cruzrickelvi@gmail.com>
2026-06-20 12:55:01 -0400

    added simple logic to CategoryService

--- src/main/java/org/yearup/repository/CategoryRepository.java
+++ src/main/java/org/yearup/repository/CategoryRepository.java
@@ -3,8 +3,12 @@ package org.yearup.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.yearup.models.Category;
+import org.yearup.models.Product;
+
+import java.util.List;
 
 @Repository
-public interface CategoryRepository extends JpaRepository<Category, Integer>
-{
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+    //List<Category> findByCategory(Category category);
+    List<Category> findByProduct(Product product);
 }
