8a637ed033d7d8a24b894bf37a2b6a49d988c68f
RickelviC <Cruzrickelvi@gmail.com>
2026-06-24 13:49:34 -0400

    fixed user infor not getting saved

---
+++ src/main/java/org/yearup/controllers/ProfileController.java
@@ -0,0 +1,50 @@
+package org.yearup.controllers;
+
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.yearup.models.Profile;
+import org.yearup.models.User;
+import org.yearup.service.ProfileService;
+import org.yearup.service.UserService;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/profile")
+@PreAuthorize("isAuthenticated()")//must be logged in to see the profile
+@CrossOrigin
+public class ProfileController {
+    private ProfileService profileService;
+    private UserService userService;
+
+    public ProfileController(ProfileService profileService, UserService userService) {
+        this.profileService = profileService;
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public Profile getById(Principal principal) {
+        int userId = getUser(principal);
+        return profileService.getById(userId);
+    }
+
+    @PutMapping
+    public Profile updateCategory(Principal principal, @RequestBody Profile profile) {
+        int userId = getUser(principal);
+        Profile updated = profileService.updateById(userId, profile);
+
+        if (updated == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No Users with id " + userId);
+        }
+        return updated;
+    }
+    // helper to find database user by username
+    private int getUser(Principal principal) {
+        String userName = principal.getName();
+        User user = userService.getByUserName(userName);
+        return user.getId();
+    }
+}
--- src/main/java/org/yearup/service/ProfileService.java
+++ src/main/java/org/yearup/service/ProfileService.java
@@ -1,6 +1,8 @@
 package org.yearup.service;
 
 import org.springframework.stereotype.Service;
+import org.yearup.models.CartItem;
+import org.yearup.models.Product;
 import org.yearup.models.Profile;
 import org.yearup.repository.ProfileRepository;
 
@@ -15,4 +17,26 @@ public class ProfileService {
     public Profile create(Profile profile) {
         return profileRepository.save(profile);
     }
+
+    public Profile getById(int profileId) {
+        return profileRepository.findById(profileId).orElse(null);
+    }
+
+    public Profile updateById(int profileId, Profile profile){
+        Profile existing = getById(profileId);
+
+        existing.setFirstName(profile.getFirstName());
+        existing.setLastName(profile.getLastName());
+        existing.setPhone(profile.getPhone());
+        existing.setEmail(profile.getEmail());
+        existing.setAddress(profile.getAddress());
+        existing.setCity(profile.getCity());
+        existing.setState(profile.getState());
+        existing.setZip(profile.getZip());
+
+        profileRepository.save(existing);
+
+        return profile;
+    }
+
 }
