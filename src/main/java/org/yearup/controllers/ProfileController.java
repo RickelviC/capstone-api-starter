ad19bcd93396b213b1971641aad5cc0f2f1fe6c2
RickelviC <Cruzrickelvi@gmail.com>
2026-06-25 12:14:41 -0400

    added README.md

--- src/main/java/org/yearup/controllers/ProfileController.java
+++ src/main/java/org/yearup/controllers/ProfileController.java
@@ -1,6 +1,5 @@
 package org.yearup.controllers;
 
-import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
